e6e3463180d62fd97a0f3eb97059d646c57c851a
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-10-19 09:24:27 +0300

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Ado.net
+{
+    static class Config
+    {
+        public static string ConnectionString = @"Server=DESKTOP-RE44NV4\SQLEXPRESS;Database=SoftUni;Integrated Security=true";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Ado.net
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //if authentication is needed
+            //SqlCredential sqlCredential = new SqlCredential(username, password);
+            
+            //create connection between server and db with term "using" without brackets
+            using SqlConnection sqlConnection = new SqlConnection(Config.ConnectionString);
+
+            //open connection
+            sqlConnection.Open();
+
+
+            //query text
+            string employeeCntQuery = @"SELECT COUNT(*) 
+                                AS [EmployeeCount] 
+                              FROM [Employees]";
+
+            //command for execution of query text
+            SqlCommand employeeCntCommand = new SqlCommand(employeeCntQuery, sqlConnection);
+
+            //ExecuteScalar - method of SqlCommand object which returns one value
+            int employeeCount = (int)employeeCntCommand.ExecuteScalar();
+
+            string employeeInfoQuery = @"SELECT [FirstName],
+		                                        [LastName],
+		                                        [JobTitle]
+                                           FROM [Employees]";
+
+            SqlCommand employeeInfoCmd = new SqlCommand(employeeInfoQuery, sqlConnection);
+            
+            //ExecuteReader - method of SqlCommand object which returns many rows and columns as SqlDataReader type
+            using SqlDataReader employeeInfoReader = employeeInfoCmd.ExecuteReader();
+
+            //close connections regardless of "using"
+            sqlConnection.Close();
+            employeeInfoReader.Close();
+
+
+        }
+    }
+}
